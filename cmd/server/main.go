4de0b33a83ace4c918c01800203a24a321fe42a8
MehrshadFb <mehrshad.farahbakhsh@gmail.com>
2026-05-07 12:23:33 -0400

    Add gRPC server entrypoint and register RPC handlers

---
+++ cmd/server/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	xov1 "github.com/MehrshadFb/xo-grpc/gen/go/xo/v1"
+	gamesvc "github.com/MehrshadFb/xo-grpc/internal/service/game"
+	"github.com/MehrshadFb/xo-grpc/internal/service/lobby"
+	"github.com/MehrshadFb/xo-grpc/internal/service/session"
+	"github.com/MehrshadFb/xo-grpc/internal/store/memory"
+	transportgrpc "github.com/MehrshadFb/xo-grpc/internal/transport/grpc"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+func main() {
+	// Infrastructure
+	store := memory.NewStore()
+	sessions := session.NewManager()
+
+	// Services
+	lobbyService := lobby.NewService(store, sessions)
+	gameService := gamesvc.NewService(store, sessions)
+
+	// gRPC handlers
+	lobbyHandler := transportgrpc.NewLobbyHandler(lobbyService)
+	gameHandler := transportgrpc.NewGameHandler(gameService)
+
+	// gRPC server
+	grpcServer := grpc.NewServer()
+
+	// Enable grpcurl reflection support and register services
+	reflection.Register(grpcServer)
+	xov1.RegisterLobbyServiceServer(grpcServer, lobbyHandler)
+	xov1.RegisterGameServiceServer(grpcServer, gameHandler)
+
+	// Listen
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	log.Println("gRPC server listening on :50051")
+
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
